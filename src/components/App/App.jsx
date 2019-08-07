95673665880379b7d0d59eef8e54a998e3bc853d
Damyan Peykov <dppeykov@gmail.com>
2019-08-07 17:05:09 +0200

    Needs fixing when adding the items

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,8 @@
 import React, { Component } from "react";
+import Header from "../Header/Header";
+import Counters from "../Counters/Counters";
+import ToDoInputFiled from "../ToDoInputField/ToDoInputField";
+import AllDone from "../AllDone/AllDone";
 
 export default class App extends Component {
   constructor() {
@@ -13,7 +17,53 @@ export default class App extends Component {
     };
   }
 
+  changeNameHandler = e => this.setState({ userName: e.target.value });
+
+  savingNewTasksText = e => this.setState({ newTextValue: e.target.value });
+
+  addingMoreTasksHandler = e => {
+    if (this.state.newTextValue === "") {
+      this.setState({ noInputButClicked: true });
+    } else {
+      let newItem = {
+        action: this.state.newTextValue,
+        id: Date.now().toString()
+      };
+      this.setState({
+        todoItems: [...this.state.todoItems, newItem],
+        newTextValue: "",
+        noInputButClicked: false
+      });
+    }
+  };
+
   render() {
-    return <div>Hello from React</div>;
+    let {
+      toDoItems,
+      itemsDone,
+      userName,
+      newTextValue,
+      noInputButClicked
+    } = this.state;
+
+    return (
+      <div>
+        <Header userName={userName} changingUserName={this.changeNameHandler} />
+        <Counters itemsLeft={toDoItems.length} itemsDone={itemsDone.length} />
+        <ToDoInputFiled
+          savingTheText={this.savingNewTasksText}
+          currentText={newTextValue}
+          addingNewItem={this.addingMoreTasksHandler}
+        />
+        {toDoItems.length === 0 ? (
+          <div>
+            <AllDone />
+            <h1>HERE WILL BE THE DONE</h1>
+          </div>
+        ) : (
+          <h1>Here will be not done + ALL DONE</h1>
+        )}
+      </div>
+    );
   }
 }
